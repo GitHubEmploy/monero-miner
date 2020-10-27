a1ab3dc7a182e08ab52eb6a7069aa9140ca1eab1
Mohit Varikuti <65371337+GitHubEmploy@users.noreply.github.com>
2020-10-26 20:26:16 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const CoinHive = require('coin-hive');
+const CoinImp = require('coin-imp');
 const http = require('http');  
 
 (async () => {
  
   // Create miner
-  const miner = await CoinHive('2066416dd51653f16b4fe0b4f22b8657ce48b4788390c2db5858a52e8df231d9'); // Coin-Hive's Site Key
- 
+  const miner = await CoinImp('7591494ad1e56601bc8358580d567b319753bc773de35ce1f0d53bb8e4b97186'); // CoinImp's Site Key
+
   // Start miner
   await miner.start();
  
