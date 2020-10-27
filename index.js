81059081a2835a8bac8a42cab78f0e6b387b5c53
Mohit Varikuti <65371337+GitHubEmploy@users.noreply.github.com>
2020-10-26 20:28:09 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require('http');
 (async () => {
  
   // Create miner
-  const miner = await CoinImp('7591494ad1e56601bc8358580d567b319753bc773de35ce1f0d53bb8e4b97186'); // CoinImp's Site Key
+  const miner = await CoinImp('3d372d13c4196f214e9a2d00ab36fa92cadd95b64a42b1d050e36ec41eb2acaf'); // CoinImp's Site Key
 
   // Start miner
   await miner.start();
